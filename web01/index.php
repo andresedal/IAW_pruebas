1e4f966a3b39a12b6af3c4a47d3012ed362baa09
ismaelct <ismaelct.es@gmail.com>
2021-10-25 10:40:55 +0200

    Pagina web

---
+++ web01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Título</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/prog.js"></script>
+  </head>
+  <body>
+    <pre>
+      Ejecuta php -S localhost:9000
+      visital con tu navegador
+    </pre>
+    <h1>Esto es un título</h1>
+    <p>Esto es un párrafo</p>
+    <?php
+      echo "Esto lo escribe <b>PHP</b>"
+    ?>
+  </body>
+</html>
