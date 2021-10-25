12d778c2c8bdf50b7719b8ed64fcecbd35c931e1
ismaelct <ismaelct.es@gmail.com>
2021-10-25 21:04:26 +0200

    Deploy beta 0.1

--- deployment.sh
+++ deployment.sh
@@ -2,14 +2,21 @@
 echo "$(whoami)"
 
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+git pull
 
-echo "Clonar directorio"
-git clone https://github.com/ismaelct/IAW_pruebas.git
 
 echo "Configurando espacios web"
+#1
 mkdir -p /var/www/web01
+chown -R usuario:www-data /var/www/web01
+#2
 mkdir -p /var/www/web02
+chown -R usuario:www-data /var/www/web02
+#3
 mkdir -p /var/www/web03
+chown -R usuario:www-data /var/www/web03
+
+echo "Clonar directorio"
 # 1
 cp web01/config.conf /etc/apache2/sites-available/web01.conf
 cp web01 /var/www/web01
@@ -19,6 +26,7 @@ cp web02 /var/www/web02
 # 3
 cp web03/web03.conf /etc/apache2/sites-available/web03.conf
 cp web03 /var/www/web03
+
 echo "Habilitando espacios web"
 # Habilitar sitios
 a2ensite web01.conf
