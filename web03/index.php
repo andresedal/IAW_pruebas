669a4aaa6d58947289f210caeddd36ac314e685b
ismaelct <ismaelct.es@gmail.com>
2021-10-26 10:31:06 +0200

    Modificacion de las webs Tue 26 Oct 2021 10:31:06 AM CEST

--- web03/index.php
+++ web03/index.php
@@ -1,21 +1,27 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Título</title>
-    <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/prog.js"></script>
-  </head>
-  <body>
-	<h1> WEB 03 </h1>
-    <pre>
-      Ejecuta php -S localhost:9000
-      visital con tu navegador
-    </pre>
-    <h1>Esto es un título</h1>
-    <p>Esto es un párrafo</p>
-    <?php
-      echo "Esto lo escribe <b>PHP</b>"
-    ?>
-  </body>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>WEB 01</title>
+		<link rel="stylesheet" href="css/estilo.css">
+		<script src="js/prog.js"></script>
+	</head>
+	<body>
+		<h1>
+			WEB 01
+		</h1>
+		<pre>
+			Ejecuta php -S localhost:9000
+			visital con tu navegador
+		</pre>
+		<h1>
+			Esto es un título
+		</h1>
+		<p>
+			Esto es un párrafo
+		</p>
+		<?php
+			echo "Esto lo escribe <b>PHP</b>"
+		?>
+	</body>
 </html>
