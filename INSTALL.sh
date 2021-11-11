6e5a4428348afb579654390352c200568adfe53a
ismaelct <ismaelct.es@gmail.com>
2021-11-11 19:21:16 +0100

    cambios

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 clear
-
+# Check4Update
+git remote update
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+    echo -e "\e[42mActualizado\e[0m"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull";
+    echo -e "\e[41mNecesita actualizar en local\e[0m"
     git pull
 elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
+    echo "\e[43mNecesita actualizar en remoto\e[0m"
 else
     echo "Diverged"
 fi
-
-# MENU awk -F: '{if($3 > 1000) {print $1}}' /etc/passwd
-
-
+echo " "
+# MENU 
 PS3='Seleccionar una opción: '
-options=("Listar Usuarios" "Crear Usuarios" "Borrar Usuarios" "salir")
+options=("Listar Usuarios" "Crear Usuarios" "Borrar Usuarios" "Opciones repositorio" "salir")
 select opt in "${options[@]}"
 do
     case $opt in
         "Listar Usuarios")
-            echo "$opt"
+            echo -e "\e[31m$opt\e[0m"
             echo "==================="
             awk -F: '{if($3 >= 1000 && $3 < 65534) {print "("$3")>"$1}}' /etc/passwd
             echo "==================="
@@ -36,6 +35,9 @@ do
             ;;
         "Borrar Usuarios")
             echo "$opt"
+            ;;
+        "Actualizar repositorio")
+            git pull
             ;;
         "salir")
             break
