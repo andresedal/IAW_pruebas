e534097a7e7ef0500b9e00bfb317236131bb6399
ismaelct <ismaelct.es@gmail.com>
2021-11-11 12:39:09 +0100

    script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# if [ $status = 1 ]; then
-#    do 
-
-for 0 in $status
-do
-# https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
+
 if [ $LOCAL = $REMOTE ]; then
-    echo "Repositorio Actualizado."
-    status=0
+    echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Necesario actualizar repositorio."
-    chmod +x ./update.sh
-    ./update.sh
-    status=1
+    echo "Need to pull";
+    git pull
 elif [ $REMOTE = $BASE ]; then
-    echo "Falta subir cambios."
+    echo "Need to push"
 else
     echo "Diverged"
-fi 
-done
+fi
