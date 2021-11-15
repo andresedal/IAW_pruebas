a0aec1bad6b41a7a0f8fbea25ed9592fbeb19ec6
ismaelct <ismaelct.es@gmail.com>
2021-11-15 11:56:37 +0100

    Cambios create user

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read nombre
+read -p 'Escriba el nombre completo: ' nombrecompleto
+read -p 'Escriba el nombre de usuario: ' nombre
 nombreunico=$nombre$RANDOM
 echo "Nombre de usuario $nombreunico"
 psw1=$(date +%s | sha256sum | base64 | head -c 4)
@@ -8,8 +9,16 @@ pswd="$psw1""$psw2""$RANDOM"
 
 echo "Contraseña auto generada ${pswd:0:1}*******${pswd:12:13}"
 
-adduser
+# FALTA ADDUSER
+
+adduser --gecos "$nombrecompleto" --no-create-home --home /var/www/$nombreunico --shell /bin/false $nombreunico
 
 mkdir /var/www/$nombreunico/{web,blog,files}
 chown root:root /var/www/$nombreunico
-chmod 750 /var/www/
+chmod 755 /var/www/
+chown -R $usuariounico:$usuariounico /var/www/$nombreunico/*
+
+echo "Match User $usuariounico
+        ChrootDirectory /var/www/$usuariounico/
+        PasswordAuthentication yes"\n >> /etc/ssh/sshd_config
+
